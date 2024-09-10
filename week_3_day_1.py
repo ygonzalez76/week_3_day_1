fe65b27aa8d4c5619a8c897b217504ac5bc3b831
Gonzalez <ygonzalez76@cps.k12.il.us>
2024-09-10 15:03:20 -0500

    whatever changes you worked on

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -50,29 +50,45 @@
 # # Review
 # create variables for the following :
 # 1. age
+age = 16 #integer string
 # 2. name
+name = "Yailyn" #string variable
 # 3. song
+song = "Chicken wing" #string variable
 # 4. food
+food = "balogna" #string variable
 # 5. number
+number = 21 #integer variable
+
 
 
 # #now include the variables you just made print in the following...
 
 
 # Once upon a time, there was a [age] old coder named [name].
-
+print("Once upon a time, there was a " + str(age) + " year old coder named " + name + ".")
 
 # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+print((name) + " liked to hum the song " + (song) + " while coding. It was so annoying that their teammates would throw " + (food) + " until " + (name) + " would stop singing.")
 
 
-# Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
+print("Still, " + (name) + " was the best coder on the team and could write " + str(number) + " lines of code every day. Maybe " + (song) + " was " + (name) + "’s secret power?")
+
+
 ##########################################################################################
 
 
 
+number1= 100
+number2= 200
+number3= 300
+number4= 400
+number5= 500
+#put all of these into a sentence
 
+# print(str(number) + "." + str(number1) + "," + str(number2) + str(number4) + "," + str(number5))
 
-
+print(f"{number}.{number1},{number2},{number3},{number4},{number5})
 
 
 
